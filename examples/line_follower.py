35386646ad49c126dcb33610bb71ce80675edbc9
Jiangpeng LI <jiangpengli86@gmail.com>
2023-08-14 18:04:30 +0800

    Fix python version error
    Supplementary What:
    
    None.
    
    Why:
    
    Previously, all the code within this library is written in Python 2.6.
    However, we use Python 3 nowadays.
    
    How:
    
    Change the code syntax and change the imported library names.
    
    Tags:
    
    All.

--- examples/line_follower.py
+++ examples/line_follower.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 #
 #       line_follower.py
-#       
+#
 #       Copyright 2011 Manuel Martín Ortiz <mmartinortiz@gmail.com>
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 2 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       You should have received a copy of the GNU General Public License
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -36,106 +36,110 @@ import re
 
 # You can use this dictionary to asociate an ePuck ID with its MAC Address
 epucks = {
-	'1797' : '10:00:E8:6C:A2:B6',
-	'1903' : '10:00:E8:6C:A1:C7'
+    '3147': '10:00:E8:AD:78:18',
 }
 
+
 def log(text):
-	"""	Show @text in standart output with colors """
-	
-	blue = '\033[1;34m'
-	off = '\033[1;m'
-	
-	print(''.join((blue, '[Log] ', off, str(text))))
-		
+    """	Show @text in standart output with colors """
+
+    blue = '\033[1;34m'
+    off = '\033[1;m'
+
+    print((''.join((blue, '[Log] ', off, str(text)))))
+
+
 def error(text):
-	red = '\033[1;31m'
-	off = '\033[1;m'
-	
-	print(''.join((red, '[Error] ', off, str(text))))
-	
+    red = '\033[1;31m'
+    off = '\033[1;m'
+
+    print((''.join((red, '[Error] ', off, str(text)))))
+
+
 def main(mac):
-	
-	global_speed = 180
-	fs_speed = 0.6
-	threshold = 1000
+    global_speed = 180
+    fs_speed = 0.6
+    threshold = 1000
 
-	log('Conecting with ePuck')
-	try:
-		# First, create an ePuck object.
-		# If you want debug information:
-		#~ robot = ePuck(mac, debug = True)
-		# else:
-		robot = ePuck(mac)
+    log('Conecting with ePuck')
+    try:
+        # First, create an ePuck object.
+        # If you want debug information:
+        # ~ robot = ePuck(mac, debug = True)
+        # else:
+        robot = ePuck(mac)
 
-		# Second, connect to it
-		robot.connect()
-		
-		# You can enable various sensors at the same time. Take a look to
-		# to DIC_SENSORS for know the name of the sensors
-		robot.enable('floor', 'proximity')
+        # Second, connect to it
+        robot.connect()
 
-		leds_on = [0] * 8
-		log('Conection complete. CTRL+C to stop')
-		log('Library version: ' + robot.version)
-	
-		times_got = []
-	except Exception, e:
-		error(e)
-		sys.exit(1)
-		
-	try:
-		while True:
-			# Important: when you execute 'step()', al sensors
-			# and actuators are updated. All changes you do on the ePuck
-			# will be effectives after this method, not before
-			robot.step()
-			
-			# Now, we can get updated information from the sensors
-			floor_sensors = robot.get_floor_sensors()
-			prox_sensors = robot.get_proximity()
-			
-			# line_follower
-			delta = floor_sensors[2] - floor_sensors[0]
-			l_speed = global_speed - fs_speed * delta
-			r_speed = global_speed + fs_speed * delta
+        # You can enable various sensors at the same time. Take a look to
+        # to DIC_SENSORS for know the name of the sensors
+        robot.enable('floor', 'proximity')
 
-			# Now, we set the motor speed. Remember that we need to execute 'step()'
-			# for make this command effective
-			robot.set_motors_speed(l_speed, r_speed)
-			
-			# leds on/off
-			for index, s in enumerate(prox_sensors):
-				if int(s) > threshold and leds_on[index] == 0:
-					# Switch On
-					robot.set_led(index, 1)
-					leds_on[index] = 1
-				elif int(s) < threshold and leds_on[index] == 1:
-					# Switch Off
-					robot.set_led(index, 0)
-					leds_on[index] = 0
-					
-	except KeyboardInterrupt:
-		log('Stoping the robot. Bye!')
-		robot.close()
-		sys.exit()
-	except Exception, e:
-		error(e)
+        leds_on = [0] * 8
+        log('Conection complete. CTRL+C to stop')
+        log('Library version: ' + robot.version)
 
-	return 0
+        times_got = []
+    except Exception as e:
+        error(e)
+        sys.exit(1)
+
+    try:
+        while True:
+            # Important: when you execute 'step()', al sensors
+            # and actuators are updated. All changes you do on the ePuck
+            # will be effectives after this method, not before
+            robot.step()
+
+            # Now, we can get updated information from the sensors
+            floor_sensors = robot.get_floor_sensors()
+            prox_sensors = robot.get_proximity()
+
+            # line_follower
+            delta = floor_sensors[2] - floor_sensors[0]
+            l_speed = global_speed - fs_speed * delta
+            r_speed = global_speed + fs_speed * delta
+
+            # Now, we set the motor speed. Remember that we need to execute 'step()'
+            # for make this command effective
+            robot.set_motors_speed(l_speed, r_speed)
+
+            # leds on/off
+            for index, s in enumerate(prox_sensors):
+                if int(s) > threshold and leds_on[index] == 0:
+                    # Switch On
+                    robot.set_led(index, 1)
+                    leds_on[index] = 1
+                elif int(s) < threshold and leds_on[index] == 1:
+                    # Switch Off
+                    robot.set_led(index, 0)
+                    leds_on[index] = 0
+
+    except KeyboardInterrupt:
+        log('Stoping the robot. Bye!')
+        robot.close()
+        sys.exit()
+    except Exception as e:
+        error(e)
+
+    return 0
+
 
 if __name__ == '__main__':
-	X = '([a-fA-F0-9]{2}[:|\-]?){6}'
-	if len(sys.argv) < 2:
-		error("Usage: " + sys.argv[0] + " ePuck_ID | MAC Address")
-		sys.exit()
-	robot_id = sys.argv[1]
-	
-	if epucks.has_key(robot_id):
-		main(epucks[robot_id])
-		
-	elif re.match(X, robot_id) != 0:
-		main(robot_id)
-	
-	else:
-		error('You have to indicate the MAC direction of the robot')
+    X = '([a-fA-F0-9]{2}[:|\-]?){6}'
+    # if len(sys.argv) < 2:
+    #     error("Usage: " + sys.argv[0] + " ePuck_ID | MAC Address")
+    #     sys.exit()
+    # robot_id = sys.argv[1]
+    robot_id = '3147'
+
+    if robot_id in epucks:
+        main(epucks[robot_id])
+
+    elif re.match(X, robot_id) != 0:
+        main(robot_id)
+
+    else:
+        error('You have to indicate the MAC direction of the robot')
+
--- examples/photo_taker.py
+++ examples/photo_taker.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 #
 #       line_follower.py
-#       
+#
 #       Copyright 2010 Manuel Martín Ortiz <mmartinortiz@gmail.com>
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 2 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       You should have received a copy of the GNU General Public License
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -32,110 +32,114 @@ import time
 
 # You can use this dictionary to asociate an ePuck ID with its MAC Address
 epucks = {
-	'1797' : '10:00:E8:6C:A2:B6',
-	'1903' : '10:00:E8:6C:A1:C7'
+    '3147': '10:00:E8:AD:78:18',
 }
 
+
 def log(text):
-	"""	Show @text in standart output with colors """
-	
-	blue = '\033[1;34m'
-	off = '\033[1;m'
-	
-	print(''.join((blue, '[Log] ', off, str(text))))
-		
+    """	Show @text in standart output with colors """
+
+    blue = '\033[1;34m'
+    off = '\033[1;m'
+
+    print((''.join((blue, '[Log] ', off, str(text)))))
+
+
 def error(text):
-	red = '\033[1;31m'
-	off = '\033[1;m'
-	
-	print(''.join((red, '[Error] ', off, str(text))))
-	
+    red = '\033[1;31m'
+    off = '\033[1;m'
+
+    print((''.join((red, '[Error] ', off, str(text)))))
+
+
 def main(mac):
-	
-	global_speed = 180
-	fs_speed = 0.6
-	threshold = 1000
+    global_speed = 180
+    fs_speed = 0.6
+    threshold = 1000
 
-	print('Connecting with the ePuck')
-	try:
-		# First, create an ePuck object.
-		# If you want debug information:
-		#~ robot = ePuck(mac, debug = True)
-		# ele:
-		robot = ePuck(mac)
+    print('Connecting with the ePuck')
+    try:
+        # First, create an ePuck object.
+        # If you want debug information:
+        # ~ robot = ePuck(mac, debug = True)
+        # ele:
+        robot = ePuck(mac)
 
-		# Second, connect to it
-		robot.connect()
+        # Second, connect to it
+        robot.connect()
 
-		# You can enable various sensors at the same time. Take a look to
-		# to DIC_SENSORS for know the name of the sensors
-		robot.enable('camera', 'motor_position')
+        # You can enable various sensors at the same time. Take a look to
+        # to DIC_SENSORS for know the name of the sensors
+        robot.enable('camera', 'motor_position')
 
-		# We have to set the camera parameters
-		robot.set_camera_parameters('RGB_365', 40, 40, 8)
-		
-		log('Conection complete. CTRL+C to stop')
-		log('Library version: ' + robot.version)
-	
-	except Exception, e:
-		error(e)
-		sys.exit(1)
-		
-	try:
-		counter = 0
-		while True:
-			# Important: when you execute 'step()', al sensors
-			# and actuators are updated. All changes you do on the ePuck
-			# will be effectives after this method, not before
-			robot.step()
+        # We have to set the camera parameters
+        robot.set_camera_parameters('RGB_365', 40, 40, 8)
 
-			image = robot.get_image()
+        log('Conection complete. CTRL+C to stop')
+        log('Library version: ' + robot.version)
 
-			if image != None:
-				# Do something with the image
-				robot.save_image('ePuck-' + str(counter) + '.png')
-				counter += 1
+    except Exception as e:
+        error(e)
+        sys.exit(1)
 
-			# Set the motors speed and position
-			robot.set_motors_speed(100,-100)
-			robot.set_motor_position(0,0)
+    try:
+        counter = 0
+        while True:
+            # Important: when you execute 'step()', al sensors
+            # and actuators are updated. All changes you do on the ePuck
+            # will be effectives after this method, not before
+            robot.step()
 
-			# Make a 'step()' and the robot will move
-			robot.step()
+            image = robot.get_image()
 
-			while robot.get_motor_position()[0] < 270:
-				# Keep turning on itself
-				robot.step()
+            if image != None:
+                # Do something with the image
+                robot.save_image('ePuck-' + str(counter) + '.png')
+                counter += 1
 
-			# Stop the robot (don't forget the 'step()')
-			robot.stop()
-			robot.step()
+            # Set the motors speed and position
+            robot.set_motors_speed(100, -100)
+            robot.set_motor_position(0, 0)
 
-			# Sleep, otherwise we will not see the stop
-			time.sleep(1)
-					
-	except KeyboardInterrupt:
-		log('Stoping the robot. Bye!')
-		robot.close()
-		sys.exit()
-	except Exception, e:
-		error(e)
+            # Make a 'step()' and the robot will move
+            robot.step()
 
-	return 0
+            while robot.get_motor_position()[0] < 270:
+                # Keep turning on itself
+                robot.step()
+
+            # Stop the robot (don't forget the 'step()')
+            robot.stop()
+            robot.step()
+
+            # Sleep, otherwise we will not see the stop
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        log('Stoping the robot. Bye!')
+        robot.close()
+        sys.exit()
+    except Exception as e:
+        error(e)
+
+    return 0
+
 
 if __name__ == '__main__':
-	X = '([a-fA-F0-9]{2}[:|\-]?){6}'
-	if len(sys.argv) < 2:
-		error("Usage: " + sys.argv[0] + " ePuck_ID | MAC Address")
-		sys.exit()
-	robot_id = sys.argv[1]
-	
-	if epucks.has_key(robot_id):
-		main(epucks[robot_id])
-		
-	elif re.match(X, robot_id) != 0:
-		main(robot_id)
-	
-	else:
-		error('You have to indicate the MAC direction of the robot')
-		
+    X = '([a-fA-F0-9]{2}[:|\-]?){6}'
+    # if len(sys.argv) < 2:
+    #     error("Usage: " + sys.argv[0] + " ePuck_ID | MAC Address")
+    #     sys.exit()
+    # robot_id = sys.argv[1]
+    robot_id = '3147'
+
+    if robot_id in epucks:
+        main(epucks[robot_id])
+
+    elif re.match(X, robot_id) != 0:
+        main(robot_id)
+
+    else:
+        error('You have to indicate the MAC direction of the robot')
+
+
